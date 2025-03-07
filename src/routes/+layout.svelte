ea196c6c1563f1c05e2c2e216d39fc99da33073e
Shriji <shrijike@gmail.com>
2025-03-07 15:20:58 -0500

    Move authentication check to app layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import pb from '$lib/vendor/pocketbase';
 	import '../app.css';
 	let { children } = $props();
 	import { ModeWatcher } from 'mode-watcher';
-
-	$effect(() => {
-		if (!pb.authStore.token) goto('/login');
-	});
 </script>
 
 <ModeWatcher />
