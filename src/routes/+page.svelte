73e7966a9f9f76507f14dffda4898dac369367f3
Shriji <shrijike@gmail.com>
2025-03-07 03:15:29 -0500

    add pocketbae interactions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button/index.js';
+	import pb from '$lib/vendor/pocketbase';
+
+	const getData = async () => {
+		const record = await pb.collection('test').getOne('408a03k76nkg4et');
+		console.log(record);
+	};
 </script>
 
-<Button>Click me</Button>
+<Button onclick={getData}>Click me</Button>
