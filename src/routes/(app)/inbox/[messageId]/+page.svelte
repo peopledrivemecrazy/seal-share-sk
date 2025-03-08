1bb97c32d5d6f7ef06a3c82430b3a49df0f551a4
Chee Kian Teoh <teoh4770@mylaurier.ca>
2025-03-08 14:27:27 -0500

    update single record view

--- src/routes/(app)/inbox/[messageId]/+page.svelte
+++ src/routes/(app)/inbox/[messageId]/+page.svelte
@@ -4,6 +4,8 @@
 
 	let { data }: { data: PageData } = $props();
 
+	console.log(data);
+
 	const downloadFile = () => {
 		if (!data.message?.files) return;
 
@@ -20,16 +22,32 @@
 	};
 </script>
 
-<Button onclick={downloadFile}>Download</Button>
-<a href={'/inbox'}>Back to inbox</a>
 {#if data.message}
-	<h1>{data.message.id}</h1>
+	<div class="px-4">
 
-	<hr />
-	<pre>
-	{data.message.decrypted_message?.decrypted_message}
-	</pre>
-	<hr />
-{:else}
+	<h1 class="text-3xl font-bold text-center my-6">Message</h1>
+
+	<div class="space-y-4">
+		<div class="space-y-1">
+			<p class="text-lg">
+				<span class="font-medium">From:</span> {data.message?.sender_name}
+			</p>
+			<p class="text-lg">
+				<span class="font-medium">On:</span> {data.message?.created}
+			</p>
+			<p class="text-lg font-medium">Message from {data.message?.sender_name}</p>
+		</div>
+
+		<div class="bg-[#1a1a1a] p-4 rounded-md min-h-24">
+			<p class="text-white">{data.message.decrypted_message?.decrypted_message || "No message here"}</p>
+		</div>
+	</div>
+
+	<div class="mt-8 space-x-2">
+		<Button onclick={downloadFile}>Download</Button>
+		<a href={'/inbox'}>Back to inbox</a>
+	</div>
+	</div>
+	{:else}
 	<p>Loading...</p>
 {/if}
