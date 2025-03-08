35c61a4777a923e37d8e02dc515cfb6fa5430abe
Chee Kian Teoh <teoh4770@mylaurier.ca>
2025-03-08 13:18:59 -0500

    Include table in the inbox view

--- src/routes/(app)/inbox/+page.svelte
+++ src/routes/(app)/inbox/+page.svelte
@@ -1,22 +1,7 @@
 <script lang="ts">
-	import { getMessages } from '$lib/vendor/pocketbase/message';
-	import type { RecordModel } from 'pocketbase';
-	import { onMount } from 'svelte';
-	let messages: RecordModel[] = $state([]);
-
-	onMount(async () => {
-		messages = await getMessages();
-	});
+	import DataTable from "../inbox/data-table.svelte";
 </script>
 
-<div class="flex items-center justify-end gap-2 p-2">
-	<ul>
-		{#each messages as message}
-			<li>
-				<a href={`/inbox/${message.id}`}>
-					{message.id} received {message.created}
-				</a>
-			</li>
-		{/each}
-	</ul>
+<div class="container mx-auto py-10">
+	<DataTable />
 </div>
